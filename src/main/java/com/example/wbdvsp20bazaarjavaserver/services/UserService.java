a7dbc727b67368650f815e886d65fa9675eed610
kevinliang43 <kevinliang43@gmail.com>
2020-03-31 00:26:34 -0400

    Added User Service.

---
+++ src/main/java/com/example/wbdvsp20bazaarjavaserver/services/UserService.java
@@ -0,0 +1,47 @@
+package com.example.wbdvsp20bazaarjavaserver.services;
+import com.example.wbdvsp20bazaarjavaserver.models.User;
+import com.example.wbdvsp20bazaarjavaserver.repositories.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User createUser(User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAllUsers();
+    }
+
+    public User findUserById(int uid) {
+        return userRepository.findUserById(uid);
+    }
+
+    public int deleteUser(int uid) {
+        try {
+            userRepository.deleteById(uid);
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+
+    public int updateUser(int uid, User newUser) {
+        try {
+            userRepository.save(newUser);
+            return 1;
+        } catch (Exception e) {
+            System.out.println(e);
+            return 0;
+        }
+    }
+}
