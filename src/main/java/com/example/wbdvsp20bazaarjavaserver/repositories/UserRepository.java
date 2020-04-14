4e3532dfa44d2bae174994c49172d101035949d9
kevinliang43 <kevinliang43@gmail.com>
2020-04-14 01:22:59 -0400

    Reverted changes.

--- src/main/java/com/example/wbdvsp20bazaarjavaserver/repositories/UserRepository.java
+++ src/main/java/com/example/wbdvsp20bazaarjavaserver/repositories/UserRepository.java
@@ -22,7 +22,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("SELECT user FROM User user")
     public List<User> findAllUsers();
 
-    @Query("SELECT user FROM User user WHERE user.username= BINARY :username AND user.password= BINARY :password")
+    @Query("SELECT user FROM User user WHERE user.username=:username AND user.password=:password")
     public User findUserByCredentials(
             @Param("username") String username,
             @Param("password") String password
