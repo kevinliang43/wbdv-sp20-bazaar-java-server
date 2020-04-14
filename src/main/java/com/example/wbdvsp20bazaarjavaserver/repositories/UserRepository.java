6175f14a76f859beb5e1fa3464fd98ee3d57d4a4
kevinliang43 <kevinliang43@gmail.com>
2020-04-14 00:54:41 -0400

    Fixed Error with case sensitive matching.

--- src/main/java/com/example/wbdvsp20bazaarjavaserver/repositories/UserRepository.java
+++ src/main/java/com/example/wbdvsp20bazaarjavaserver/repositories/UserRepository.java
@@ -22,7 +22,7 @@ public interface UserRepository extends CrudRepository<User, Integer> {
     @Query("SELECT user FROM User user")
     public List<User> findAllUsers();
 
-    @Query("SELECT user FROM User user WHERE user.username=:username COLLATE Latin1_General_CS_AS AND user.password=:password COLLATE Latin1_General_CS_AS")
+    @Query("SELECT user FROM User user WHERE user.username= BINARY :username AND user.password= BINARY :password")
     public User findUserByCredentials(
             @Param("username") String username,
             @Param("password") String password
