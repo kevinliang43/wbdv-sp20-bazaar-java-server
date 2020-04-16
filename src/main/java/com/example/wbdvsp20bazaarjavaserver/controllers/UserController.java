df2171654586a099c976bfb6bf1fa84e9425122d
kevinliang43 <kevinliang43@gmail.com>
2020-04-16 00:32:49 -0400

    Updated debugging.

--- src/main/java/com/example/wbdvsp20bazaarjavaserver/controllers/UserController.java
+++ src/main/java/com/example/wbdvsp20bazaarjavaserver/controllers/UserController.java
@@ -75,10 +75,10 @@ public class UserController {
             this.service.updateUser(uid, updatedUser); // TODO: Send different response if update fails on severside.
             // IF IT WORKS
             System.out.println("BEFORE");
-            System.out.println(session);
+            System.out.println(session.getAttribute("profile"));
             session.setAttribute("profile", updatedUser);
             System.out.println("AFTER");
-            System.out.println(session);
+            System.out.println(session.getAttribute("profile"));
             // TODO: if it doesnt work, handle the case here
 
             //Return
